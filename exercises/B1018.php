baa5c56890e1d9d05e963158964d0334adb9cd3e
KuiK-San <thegui4000@gmail.com>
2022-07-16 11:48:48 -0300

    Update B1018.php

--- exercises/B1018.php
+++ exercises/B1018.php
@@ -1,3 +1,15 @@
 <?php
+$valor_notas = array(100, 50, 20, 10, 5, 2, 1);
+$valor = readline();
 
+echo $valor . "\n";
+
+for($x = 0; $x < 7; $x++){
+
+    $quantidade = $valor / $valor_notas[$x];
+
+    echo (int) $quantidade . " nota(s) de R$ " . $valor_notas[$x] . ",00\n";
+
+    $valor %= $valor_notas[$x];
+}
 ?>
