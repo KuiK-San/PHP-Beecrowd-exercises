84b271f2c0e50af4d470bc4d6eb028a26a6e7b78
Guilherme Casagrande <97366684+KuiK-San@users.noreply.github.com>
2022-07-19 16:52:09 -0300

    Created 1044

---
+++ exercises/B1044.php
@@ -0,0 +1,14 @@
+<?php
+
+$input =  explode(" ", readline());
+
+$valor1 = $input[0];
+$valor2 = $input[1];
+
+if ($valor1 % $valor2 == 0) {
+    echo "Sao Multiplos\n";
+}else{
+    echo "Nao sao Multiplos\n";
+}
+
+?>
