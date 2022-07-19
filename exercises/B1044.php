80f7aea7185040bf46583f59020d53558dad0cc9
Guilherme Casagrande <97366684+KuiK-San@users.noreply.github.com>
2022-07-19 16:54:46 -0300

    Update B1044.php

--- exercises/B1044.php
+++ exercises/B1044.php
@@ -5,7 +5,7 @@ $input =  explode(" ", readline());
 $valor1 = $input[0];
 $valor2 = $input[1];
 
-if ($valor1 % $valor2 == 0) {
+if ($valor1 % $valor2 == 0 || $valor2 % $valor1 == 0) {
     echo "Sao Multiplos\n";
 }else{
     echo "Nao sao Multiplos\n";
