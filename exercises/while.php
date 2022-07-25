590580baa48bfbd6076d82940e7793e4aa19f414
Guilherme Casagrande <thegui4000@gmail.com>
2022-07-25 16:12:13 -0300

    adicionando quebra de linha

--- exercises/while.php
+++ exercises/while.php
@@ -3,7 +3,7 @@ echo "Digite um Valor ";
 $valor = readline();
 
 while ($valor != 0) {
-    echo "echo o Valor digitado foi $valor";
+    echo "echo o Valor digitado foi $valor\n";
     echo "Digite outro valor: ";
     $valor = readline();
 }
