d3167ac65bfc9df8171f346f2da368136ca18ade
Guilherme Casagrande <thegui4000@gmail.com>
2022-07-26 16:35:42 -0300

    Accepted

--- exercises/B1154.php
+++ exercises/B1154.php
@@ -1,3 +1,14 @@
 <?php
+$qnt_idade = 0;
+$soma = 0;
+$input = readline();
+while($input >= 0){
+    $soma = $input + $soma;
+    $qnt_idade++;
+    $input = readline();
+}
+
+$media = $soma / $qnt_idade;
+echo number_format($media, 2, ".", "") . "\n";
 
 ?>
