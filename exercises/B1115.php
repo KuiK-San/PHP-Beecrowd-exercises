328277f21296b12adaaf5bf7c60a31e1f03fac5b
Guilherme Casagrande <thegui4000@gmail.com>
2022-07-26 14:29:49 -0300

    created B1115.php

---
+++ exercises/B1115.php
@@ -0,0 +1,21 @@
+<?php
+$eixos = explode(" ", readline());
+$x = $exios[0];
+$y = $eixos[1];
+
+while($x != 0 && $y != 0){
+    if ( $x > 0 && $y > 0) {
+        echo "primeiro\n";
+    }else if($x < 0 && $y > 0){
+        echo "segundo\n";
+    }else if($x < 0 && $y < 0){
+        echo "terceiro\n";
+    }
+    else if($x >0 && $y < 0){
+        echo "quarto\n";
+    }
+    $eixos = explode(" ", readline());
+    $x = $exios[0];
+    $y = $eixos[1];
+}
+?>
